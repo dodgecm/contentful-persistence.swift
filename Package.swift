fb3e046a7db13b85cf55383947afaabe1dd5b9ab
Chris Dodge <christopher.dodge@rallyhealth.com>
2021-05-27 16:56:34 -0400

    Do a better job of pointing to the fixed commit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
             targets: ["ContentfulPersistence"])
     ],
     dependencies: [
-        .package(url: "https://github.com/dodgecm/contentful.swift", .branch("fix-linking-crash-swift-5-4"))
+        .package(url: "https://github.com/dodgecm/contentful.swift", .upToNextMajor(from: "5.5.1"))
     ],
     targets: [
         .target(
